9401fc8882e71d883740ecc1a4a036933437940c
Rajab Karume <rkarume@gmail.com>
2023-06-19 10:02:14 +0300

    ✨added max length to text area

--- src/app/components/input/page.js
+++ src/app/components/input/page.js
@@ -25,7 +25,7 @@ export default function Input({user}) {
     return (
         <div className=" flex justify-center align-middle w-screen h-96 col  ">
           <form onSubmit={hanndleSubmit}  className="flex flex-col justify-center align-middle w-screen h-full col mx-auto  ">
-            <textarea value={newentry} onChange={(e)=>setNewentry(e.target.value)} className="w-3/5 h-1/2 rounded-xl outline-4 bg-slate-200 border-4 text-black mx-auto mt-10 pl-2  " placeholder="Enter your text here"/>
+            <textarea maxLength={500} value={newentry} onChange={(e)=>setNewentry(e.target.value)} className="w-3/5 h-1/2 rounded-xl outline-4 bg-slate-200 border-4 text-black mx-auto mt-10 pl-2  " placeholder="Enter your text here"/>
             <button type="submit" className="w-28 hover:bg-slate-900 rounded-xl bg-slate-800 text-white font-bold mx-auto mt-10 ">Submit</button>
           </form>
       </div>
