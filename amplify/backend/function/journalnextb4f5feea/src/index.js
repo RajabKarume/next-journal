97637034e9b649b3938aae7182da2fab9e4f292a
Rajab Karume <rkarume@gmail.com>
2023-06-20 18:35:56 +0300

    🐛removed get and delete request

--- amplify/backend/function/journalnextb4f5feea/src/index.js
+++ amplify/backend/function/journalnextb4f5feea/src/index.js
@@ -2,8 +2,6 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const {
   DynamoDBDocumentClient,
   PutCommand,
-  ScanCommand,
-  DeleteCommand,
 } = require("@aws-sdk/lib-dynamodb");
 
 const client = new DynamoDBClient({});
@@ -21,47 +19,26 @@ exports.handler = async (event, context) => {
   };
 
   try {
-    switch (event.httpMethod) {
-      case "GET":
-        const result = await dynamo.send(new ScanCommand({ TableName: tableName }));
-        body = result.Items;
-        break;
-      case "POST":
-        const { id, email, newentry } = JSON.parse(event.body).formdata;
-        const item = {
-          id,
-          email,
-          newentry,
-        };
-        
-        await dynamo.send(
-          new PutCommand({
-            TableName: tableName,
-            Item: item,
-          })
-        );
+    const { id, email, newentry } = JSON.parse(event.body).formdata;
+    const item = {
+      id,
+      email,
+      newentry,
+    };
+    
+    await dynamo.send(
+      new PutCommand({
+        TableName: tableName,
+        Item: item,
+      })
+    );
 
-        body = item;
-        statusCode = 201; // Created
-        break;
-      case "DELETE":
-        await dynamo.send(
-          new DeleteCommand({
-            TableName: tableName,
-            Key: {
-              id: event.pathParameters.id,
-            },
-          })
-        );
-        body = `Deleted item ${event.pathParameters.id}`;
-        break;
-      default:
-        throw new Error(`Unsupported route: "${event.httpMethod} ${event.resource}"`);
+    body = item;
+    statusCode = 201; // Created
+    }catch (err) {
+      statusCode = 400;
+      body = err.message;
     }
-  } catch (err) {
-    statusCode = 400;
-    body = err.message;
-  }
 
   return {
     statusCode,
